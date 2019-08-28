092386726b72c061e7ca16be0673c7eba726947d
yehuda81 <44754603+yehuda81@users.noreply.github.com>
2019-08-28 15:21:38 +0300

    Add files via upload

---
+++ ToExam5/Order.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToExam5
+{
+    class Order
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+        public string Email { get; set; }
+        public bool IsMember { get; set; }
+        public float OrderValue { get; set; }
+        public Myenum OrderStatus { get; set; }
+        public List<string> MyEnumValues = new List<string>
+        {
+            "None",
+            "Shipped",
+            "New",
+            "Processing",
+            "Received"
+        };
+
+        public Order(int id, string firstName, string lastName, int age, string email, bool isMember, float orderValue, Myenum orderStatus)
+        {
+            Id = id;
+            FirstName = firstName;
+            LastName = lastName;
+            Age = age;
+            Email = email;
+            IsMember = isMember;
+            OrderValue = orderValue;
+            OrderStatus = orderStatus;
+        }
+        public enum Myenum
+        {
+            
+            None = 1, Shipped = 2, New = 3, Processing = 4, Received = 5
+        }
+
+    }
+}
---
+++ ToExam5/UserControlCustomize.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ToExam5
+{
+    /// <summary>
+    /// Interaction logic for UserControlCustomize.xaml
+    /// </summary>
+    public partial class UserControlCustomize : UserControl
+    {
+        List<Order> orders = new List<Order>();
+        public UserControlCustomize()
+        {
+            InitializeComponent();
+            this.DataContext = this;
+            orders.Add(new Order(1, "Yehuda", "Reisner", 38, "yudale81@gmail.com", true, 33.4f, Order.Myenum.New));
+            orders.Add(new Order(2, "Sivan", "Reisner", 35, "Sivanbo55@gmail.com", false, 22.1f, Order.Myenum.Processing));
+            orders.Add(new Order(3, "Lior", "Reisner", 1, "", true, 65.3f, Order.Myenum.Shipped));
+            DG.ItemsSource = orders;
+        }
+        
+    }
+}
