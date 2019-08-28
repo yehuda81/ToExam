3594602c784b5b27d85a83bb1e1d42ce4ff3c781
yehuda81 <44754603+yehuda81@users.noreply.github.com>
2019-08-28 15:20:23 +0300

    Add files via upload

---
+++ ToExam3/CheckDataError.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToExam3
+{
+    public class CheckDataError : IDataErrorInfo
+    {
+        private string name;
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                name = value;
+            }
+        }
+        public string this[string property]
+        {
+            get
+            {
+                return GetErrorForProperty(property);
+            }
+        }
+
+        private string GetErrorForProperty(string propertyName)
+        {
+            if (Name.Length > 10)
+            {
+                return "Error";
+            }
+           
+            return string.Empty;
+        }
+
+        public string Error
+        {
+            get
+            {
+                return string.Empty;
+            }
+        }
+    }
+}
---
+++ ToExam3/ValueConverter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace ToExam3
+{
+    public class ValueConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            bool IsEnabled = true;
+            if (string.IsNullOrEmpty(value.ToString()))
+            {
+                IsEnabled = false;
+            }
+            return IsEnabled;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
