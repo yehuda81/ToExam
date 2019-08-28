85c710124c24ea735508d18eb3ff0c45cc3c139b
yehuda81 <44754603+yehuda81@users.noreply.github.com>
2019-08-28 15:19:48 +0300

    Task2

---
+++ TaskProgramming2/Window2.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace TaskProgramming2
+{
+    /// <summary>
+    /// Interaction logic for Window2.xaml
+    /// </summary>
+    public partial class Window2 : Window
+    {
+        public Window2()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Task.Run(() =>
+            {
+                for (int i = 50; i >= 0; i--)
+                {
+                    Action action = new Action(()=> tBox.Text = i.ToString());
+                    //Dispatcher.BeginInvoke(action);
+                    SafeInvoke(action);
+                    Thread.Sleep(250);
+                }
+            });
+        }
+        public void SafeInvoke(Action action)
+        {
+            if (Dispatcher.CheckAccess())
+            {
+                action.Invoke();
+                return;
+            }
+            else
+            {
+                this.Dispatcher.BeginInvoke(action);
+            }
+        }
+    }
+}
---
+++ TaskProgramming2/Window3.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace TaskProgramming2
+{
+    /// <summary>
+    /// Interaction logic for Window3.xaml
+    /// </summary>
+    public partial class Window3 : Window
+    {
+        int num;
+
+        public Window3()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            num = 5;
+            DispatcherTimer timer = new DispatcherTimer();
+            timer.Interval = TimeSpan.FromSeconds(1);
+            timer.Tick += Timer_Tick;
+            timer.Start();
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            
+            tBox.Text = num.ToString();
+            if (num > 0)
+            {
+                num--;
+            }           
+        }
+    }
+}
